24aa7346ab5d1522098089d97baebb1538fd502e
KyoRuMan <ryokuman@naver.com>
2022-07-11 15:37:00 +0900

    mod err

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,26 @@ import MainPage from "./Page/MainPage";
 import Header from "./component/Header";
 import Register from "./Page/Register";
 
-import {ReactComponent as GreenBack } from './images/greenBack.svg'
+import { ReactComponent as GreenBack } from "./images/greenBack.svg";
 
 function App() {
     return (
         <div>
-            {/* <div style={{position: 'absolute',float: "right", zIndex: -100, top: 0, right: 0}}>
+            <div
+                style={{
+                    position: "absolute",
+                    float: "right",
+                    zIndex: -100,
+                    top: 0,
+                    right: 0,
+                }}
+            >
                 <GreenBack />
-            </div> */}
+            </div>
             <Router>
-            <Header />
+                <Header />
                 <Routes>
-                <Route path=" " element={<MainPage />} />
+                    <Route path="/" element={<MainPage />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/mypage" element={<MyPage />} />

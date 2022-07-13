25a8804af4e52ee436469fefcd8ca548811e6dbb
Chaehy <chlee7670@cau.ac.kr>
2022-07-13 17:37:05 +0900

    style: change header logo

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {Button, Container, Box, Link,CssBaseline} from '@mui/material';
+import {Button, Container, Box, Link,CssBaseline, Typography} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import lottie from "lottie-web"
 
 const theme = createTheme({
     palette: {
@@ -10,27 +11,59 @@ const theme = createTheme({
     },
   });
 
+
+
+const GetLogoLottie = ()=>{
+    //lottie
+    const likecontainer = React.useRef();
+    React.useEffect(()=>{
+    lottie.loadAnimation({
+    container: likecontainer.current,
+    renderer: 'svg',
+    loop: false,
+    autoplay:true,
+    animationData:require("../images/rebikeLogoLottie2.json")
+    })
+
+ },[])
+   return(
+
+      <Link 
+        href="/mainpage"
+        sx={{position: 'absolute',left: 55, top:5, width: 160, height: 120,textDecoration:'none'}}> 
+        <Button 
+          ref={likecontainer}
+          style={{fontSize : 20, color : "black", fontWeight: 'bold', justifyContent: "flex-end",  mb: 5}}>
+          </Button>
+          <Typography
+            style={{fontSize: 8,fontWeight:"bold",display:"flex", justifyContent: "center",}}>
+            ReBIKE</Typography>     
+       </Link>
+
+       
+   )
+}
+
 function Header() {
   return (
       <ThemeProvider theme={theme}>
           <CssBaseline />
           <Container style={{position: 'absolute', backgroundColor: 'transparent', zIndex: 100, top: 0, left: 0, right: 0 }}>
-
               <Box
                   display="flex"
                   justifyContent="flex-end"
                   alignItems="flex-end"
-                  marginLeft= "auto"
-              >
-                  <Link href="/mainpage" sx={{textDecoration: 'none',fontSize : 30, color : "black", fontWeight: 'bold',mb:1,position: 'absolute',left: 55 ,fontFamily :'./font/NanumGothic-Bold.ttf'}}>
-                      HOWTRASH
-                  </Link>
+                  marginLeft= "auto"> 
+
+                  <GetLogoLottie />
+
                   <Button
                       variant="contained"
-                      sx={{fontWeight: 'bold',mt:2,mb : 2,mr:2, color:'white',backgroundColor : "#759F98"}}
+                      sx={{fontWeight: 'bold',mt:5,mb : 2,mr:2, color:'white',backgroundColor : "#759F98"}}
                   >
                   <Link href= '/mypage' sx={{textDecoration: 'none', color : "white"}}>MyPage</Link>
                   </Button>
+
                   <Button
                       variant="contained"
                       sx={{fontWeight: 'bold' ,mb : 2,color:'white',backgroundColor : "#759F98"}}

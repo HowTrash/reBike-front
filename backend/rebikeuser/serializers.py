c83b2b0315e8a55e0696cc6e9b75994e1e45231d
Chaehy <chlee7670@cau.ac.kr>
2022-07-18 00:44:45 +0900

    chore: add back master files

--- backend/rebikeuser/serializers.py
+++ backend/rebikeuser/serializers.py
@@ -4,6 +4,28 @@ from .models import user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=20)
+    alias = serializers.CharField(max_length=20)
+    email = serializers.CharField(max_length=50)
+
     class Meta:
         model = user
         fields = ['name', 'alias', 'email'] #Front에 3필드만
+
+
+#rebikeuser/serializers.py
+class UserSignupResponse(serializers.ModelSerializer):
+    class Meta:
+        model = user
+        fields = ['id']#프론트에주는 값
+
+class SignupInput(serializers.ModelSerializer):
+    # 검증부
+    email=serializers.EmailField()
+    pw=serializers.CharField(max_length=60)
+    alias=serializers.CharField(max_length=20)
+    name=serializers.CharField(max_length=20)
+    class Meta:
+        model=user
+        fields=['name', 'pw', 'alias', 'email']#실제 response할 필드
+

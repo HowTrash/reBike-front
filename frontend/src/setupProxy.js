9278ef9cc499ff96677ddbd49251d9f774b8ff67
Chaehy <chlee7670@cau.ac.kr>
2022-07-19 14:36:00 +0900

    chore: git rm -r --cached .

--- frontend/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    "/user/signup",
-    createProxyMiddleware({
-      target: "http://localhost:8080",
-    })
-  );
-};

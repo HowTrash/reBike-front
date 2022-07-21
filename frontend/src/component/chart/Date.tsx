f5edfb6b852fef46629f6995681f09fa71fe0a8d
RayLee <wooo519@naver.com>
2022-07-21 18:10:45 +0900

    fix : change chart,date to TSX

--- frontend/src/component/chart/Date.tsx
+++ frontend/src/component/chart/Date.tsx
@@ -15,34 +15,58 @@ const theme = createTheme({
   },
 });
 
+function formatNowDate(date: Date) {
+  return (
+    [
+      date.getFullYear(),
+      (date.getMonth() + 1).toString().padStart(2, '0'),
+      date.getDate().toString().padStart(2, '0')
+    ].join('-')
+  );
+} // 오늘 & 현재
+const DateNow = formatNowDate(new Date());
+
+function formatBeforeDate(date: Date) {
+  return (
+    [
+      date.getFullYear(),
+      (date.getMonth() + 1).toString().padStart(2, '0'),
+      (date.getDate() - 7).toString().padStart(2, '0')
+    ].join('-')
+  );
+} // 일주일 전
+const DateBefore = formatBeforeDate(new Date());
+
 function Dates() {
-const [StartDate, setStartDate] = React.useState(null);
-const [EndDate, setEndDate] = React.useState(null);
-const [UserData, setUserData] = React.useState(null);
+const [StartDate, setStartDate] = React.useState(DateBefore);
+const [EndDate, setEndDate] = React.useState(DateNow);
+const [UserData, setUserData] = React.useState<string[]>([]);
 
-const HandleStartChange = (date) => {
-  setStartDate(date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0'));
+const HandleStartChange = (date : Date) => {
+  const dateresult = formatNowDate(date);
+  setStartDate(dateresult);
 };
 
-const HandleEndChange = (date) => {
-  setEndDate(date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0'));
+const HandleEndChange = (date : Date) => {
+  const dateresult = formatNowDate(date);
+  setEndDate(dateresult);
 };
 
-const HandleSubmit = (event) => {
+const HandleSubmit = (event : any) => {
   event.preventDefault();
   console.log(StartDate);
   console.log(EndDate);
   {
     axios
-      .get(`http://localhost:8080/trash/mypage/users/8baaa9ae-28a5-4840-be1e-f5d0b1b6c90b/statistics/period/${StartDate}/${EndDate}`)
+      .get(`http://localhost:8080/trash/mypage/users/2146c0ba-c356-43e1-8ae2-d7334c4c03ca/statistics/period/${StartDate}/${EndDate}`)
       .then((response) => {
         // Handle success.
         setUserData(response.data);
         console.log("Well done!");
-        UserData.map((datas) => {
-          console.log(datas.cnt);
-          return UserData + 1;
-        }); // 저장된 데이터들 호출
+        UserData.map((data:any)=> {
+          console.log(data.cnt);
+          return UserData;
+        }); // 저장된 데이터들 호출, 반복 일단 멈춤
       })
       .catch((error) => {
         // Handle error.
@@ -65,26 +89,26 @@ const HandleSubmit = (event) => {
     <LocalizationProvider dateAdapter={AdapterDateFns}> 
         <DatePicker
           label="시작 날짜"
-          id="startDate"
-          name="startDate"
           inputFormat="yyyy/MM/dd"
           value={StartDate}
-          onChange={HandleStartChange}
+          onChange={HandleStartChange as any}
           renderInput={(params) => <TextField size="small" {...params} sx={{width: '35%'}} />}
         />
          <Typography color="black" fontWeight="bold" sx={{fontSize: "medium", mx: 2}}>to</Typography>
         <DatePicker
           label="종료 날짜"
-          id="endDate"
-          name="endDate"
           inputFormat="yyyy/MM/dd"
           value={EndDate}
-          onChange={HandleEndChange}
-          minDate={StartDate}
+          onChange={HandleEndChange as any}
+          minDate={StartDate as String}
           renderInput={(params) => <TextField size="small" {...params}  sx={{width: '35%'}} />}
         />
-      <Button type="submit" width="10px" variant="contained"
-      sx={{ height : 40, color : 'white',fontWeight: 'bold',fontSize:18, marginLeft:3, backgroundColor:"#759F98"}}>조회</Button>
+      <Button
+      type="submit"
+       variant="contained"
+       sx={{ height : 40, color:'white',fontWeight: 'bold',fontSize:18, marginLeft:3, backgroundColor:"#759F98"}}
+      >
+      조회</Button>
     </LocalizationProvider>
     </Grid>
     </ThemeProvider>

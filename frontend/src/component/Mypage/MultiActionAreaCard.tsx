77c9403c81d4c8316ca774b4078a52f5a3517a23
Chaehy <chlee7670@cau.ac.kr>
2022-07-23 02:56:21 +0900

    chore: add prretieer

--- frontend/src/component/Mypage/MultiActionAreaCard.tsx
+++ frontend/src/component/Mypage/MultiActionAreaCard.tsx
@@ -7,7 +7,7 @@ import {
   CardMedia,
   Typography,
   styled,
-  Link
+  Link,
 } from "@mui/material";
 
 const MyTrashcanBtn = styled(Button)(({}) => ({
@@ -20,45 +20,51 @@ const MyTrashcanBtn = styled(Button)(({}) => ({
   },
 }));
 
-
-export default function MultiActionAreaCard({image='',kind='' }) {
+export default function MultiActionAreaCard({ image = "", kind = "" }) {
   // export default function MultiActionAreaCard() {
 
-return (
-  <Card 
-      sx={{ maxWidth: 200, border: 1, margin:2  }}>
+  return (
+    <Card sx={{ maxWidth: 200, border: 1, margin: 2 }}>
       <CardActionArea>
-          <CardMedia
-              component="img"
-              height="170"
-              // image='https://picsum.photos/400/300'
-              image={image}
-              style={{padding:3, borderRadius:8}}
-          />
-          <Typography 
-              fontWeight={"bold"} fontSize={20} component="div" marginTop={1} align="center">
-              {/* 물병 */}
-              {kind}
-          </Typography>      
+        <CardMedia
+          component="img"
+          height="170"
+          // image='https://picsum.photos/400/300'
+          image={image}
+          style={{ padding: 3, borderRadius: 8 }}
+        />
+        <Typography
+          fontWeight={"bold"}
+          fontSize={20}
+          component="div"
+          marginTop={1}
+          align="center"
+        >
+          {/* 물병 */}
+          {kind}
+        </Typography>
       </CardActionArea>
-      <CardActions >
-          <MyTrashcanBtn 
-              variant="outlined"
-              sx={{
-                  margin:"auto", 
-                  fontSize:3,
-                  width:30,
-                  height:20,
-                  bgcolor : "#76F2BE", 
-                  borderColor:"#76F2BE",
-                  color: "black"}}>
-              
-              <Link href= '/mypage/howTo'
-                sx={{color: "black", textDecoration: "none"}}>
-                더보기
-              </Link>
-          </MyTrashcanBtn>
+      <CardActions>
+        <MyTrashcanBtn
+          variant="outlined"
+          sx={{
+            margin: "auto",
+            fontSize: 3,
+            width: 30,
+            height: 20,
+            bgcolor: "#76F2BE",
+            borderColor: "#76F2BE",
+            color: "black",
+          }}
+        >
+          <Link
+            href="/mypage/howTo"
+            sx={{ color: "black", textDecoration: "none" }}
+          >
+            더보기
+          </Link>
+        </MyTrashcanBtn>
       </CardActions>
-  </Card>
-);
+    </Card>
+  );
 }

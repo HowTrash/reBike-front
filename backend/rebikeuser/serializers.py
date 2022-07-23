93be83e9c0f04899cfa3e19f2cbb80c021749eb5
Chaehy <chlee7670@cau.ac.kr>
2022-07-23 23:34:12 +0900

    chore: add back master

--- backend/rebikeuser/serializers.py
+++ backend/rebikeuser/serializers.py
@@ -1,16 +1,18 @@
-from django.db.models import fields
+import uuid
 from rest_framework import serializers
 from .models import user
+import uuid
 
 
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=20)
     alias = serializers.CharField(max_length=20)
     email = serializers.CharField(max_length=50)
+    id = serializers.UUIDField(default=uuid.uuid4)
 
     class Meta:
         model = user
-        fields = ['name', 'alias', 'email']  # Front에 3필드만
+        fields = ['name', 'alias', 'email', 'id']  # Front에 3필드만
 
 
 # rebikeuser/serializers.py
@@ -26,10 +28,10 @@ class SignupInput(serializers.ModelSerializer):
     pw = serializers.CharField(max_length=60)
     alias = serializers.CharField(max_length=20)
     name = serializers.CharField(max_length=20)
-        
+
     class Meta:
         model = user
-        fields = ('email','pw','alias','name') # 프론트에주는 값
+        fields = ('email', 'pw', 'alias', 'name')  # 프론트에주는 값
 
 
 class AutoUpload(serializers.ModelSerializer):
